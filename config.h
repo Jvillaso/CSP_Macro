fc5885aff05cde77e496cefdb050c521e330bfe0
Joshua Angel Villasol <jvillaso@ececomp2.ecn.purdue.edu>
2024-06-13 17:52:07 -0400

    Cleaned

--- config.h
+++
@@ -1,6 +0,0 @@
-/* Encoders*/
-#define ENCODERS_PAD_A { E6, B5 }
-#define ENCODERS_PAD_B { D7, B4 }
-#define ENCODER_RESOLUTION 4
-
-#define DIP_SWITCH_PINS { B2, B6 }
--- rules.mk
+++
@@ -1,3 +0,0 @@
-# Enable ENCODER
-ENCODER_ENABLE = yes
-DIP_SWITCH_ENABLE = yes
